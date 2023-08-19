2e1e756e8fa801c13abd694e9e16b730d119c41a
Lucas-Braga04 <115594692+Lucas-Braga04@users.noreply.github.com>
2023-08-19 00:44:25 -0300

    Delete src directory

--- src/entities/BalanceAccount.java
+++
@@ -1,9 +0,0 @@
-package entities;
-
-public class BalanceAccount {
-	
-	public static void verificarSaldo(AccountBank account) {
-		System.out.println("Saldo atual: " + account.getSaldo());
-	}
-
-}
